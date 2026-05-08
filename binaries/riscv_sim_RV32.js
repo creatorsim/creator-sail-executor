d38ecffa124e58e6330cf92feb7c0381f88a3dae
Juan Carlos <juancarloscanoresa@gmail.com>
2026-05-08 11:23:27 +0200

    update header wasm files CREATOR Team

--- binaries/riscv_sim_RV32.js
+++ binaries/riscv_sim_RV32.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2018-2026 CREATOR Team.
+ *
+ * This file is part of CREATOR.
+ *
+ * CREATOR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CREATOR is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CREATOR.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import { setInstructions, clear_instructions, instructions, tag_instructions } from "@/core/assembler/assembler.mjs";
 import { readRegister, writeRegister, notifyRegisterUpdate } from "@/core/register/registerOperations.mjs";
 import { crex_findReg_bytag, crex_findReg } from "@/core/register/registerLookup.mjs"
